d879d8632b02ecdacceca57a485832532f9a88bd
Lucas do N. de Sousa <114512562+Yusudime@users.noreply.github.com>
2023-12-05 13:31:31 -0300

    Update servidor.py

--- Chat/servidor.py
+++ Chat/servidor.py
@@ -1,7 +1,6 @@
 from omniORB import CORBA
 import Chat__POA
 
-
 class ChatServer(Chat__POA.ChatServer):
 	def __init__(self):
 		self.messages = []
@@ -11,24 +10,19 @@ class ChatServer(Chat__POA.ChatServer):
 		texto = (f"{formatted_message} (Mensagem salva) \n")
 		arquivo = open("Conversas.txt", "a")
 		arquivo.write(texto)
-
 		print(f"Message received: {formatted_message}")
 		return "Message sent successfully."
 	def receiveMessage(self):
 		if not self.messages:
 			return "No messages available."
 		return self.messages
-
 orb = CORBA.ORB_init([], CORBA.ORB_ID)
 poa = orb.resolve_initial_references("RootPOA")
 poaManager = poa._get_the_POAManager()
 poaManager.activate()
-
 chatServant = ChatServer()
 chatObject = chatServant._this()
-
 print("Chat Server running...")
 print("Object reference:", orb.object_to_string(chatObject))
-
 orb.run()
 
